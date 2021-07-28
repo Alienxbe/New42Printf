33b5bfa8a9d7fbf4878f7d7c92bacaccfb6dbde3
Alienxbe <martin.ykman@gmail.com>
2021-07-28 20:20:35 +0200

    Added README

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 20:34:49 by mykman            #+#    #+#             */
-/*   Updated: 2021/07/27 22:02:28 by mykman           ###   ########.fr       */
+/*   Updated: 2021/07/28 20:19:26 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int main(void)
 {
-	int n[] = {125, 125, 125, -125, -125, -125, 356};
+	/*int n[] = {125, 125, 125, -125, -125, -125, 356};
 	int width[] = {10, 10, 10, 10, 10, 10, 10};
 	int minsize[] = {0, 3, 5, 0, 3, 5, 15};
 	for (int i = 0; i < 7; i++)
@@ -23,6 +23,9 @@ int main(void)
 		printf("Test %d:\n", i);
 		printf("%-10s -> %d\n", "Original", printf("|%+*.*d|\n", width[i], minsize[i], n[i]));
 		ft_printf("%-10s -> %d\n\n", "Yours", ft_printf("|%+*.*d|\n", width[i], minsize[i], n[i]));
-	}
+	}*/
+
+	printf("%.5%\n");
+	ft_printf("%.5%\n");
 	return 0;
 }
